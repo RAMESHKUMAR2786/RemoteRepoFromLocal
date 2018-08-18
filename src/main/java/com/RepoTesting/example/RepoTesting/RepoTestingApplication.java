049ab127db129c2939fe9cc0a87c2017ff82a7ea
Ramesh <rameshcse446@gmail.com>
2018-08-18 06:53:14 +0530

    -

---
+++ src/main/java/com/RepoTesting/example/RepoTesting/RepoTestingApplication.java
@@ -0,0 +1,12 @@
+package com.RepoTesting.example.RepoTesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepoTestingApplication.class, args);
+	}
+}
